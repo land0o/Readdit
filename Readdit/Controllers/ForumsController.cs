cc8b9201975a5beae4faf6b8bc86aedfa37d26b6
curtis <crutchc@yahoo.com>
2019-12-13 12:11:03 -0600

    created a forum controller

---
+++ Readdit/Controllers/ForumsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Readdit.Data;
+using Readdit.Models;
+
+namespace Readdit.Controllers
+{
+    public class ForumsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ForumsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Forums
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Forums.Include(f => f.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Forums/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var forum = await _context.Forums
+                .Include(f => f.User)
+                .FirstOrDefaultAsync(m => m.ForumId == id);
+            if (forum == null)
+            {
+                return NotFound();
+            }
+
+            return View(forum);
+        }
+
+        // GET: Forums/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id");
+            return View();
+        }
+
+        // POST: Forums/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ForumId,DateCreated,Title,Description,UserId")] Forum forum)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(forum);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", forum.UserId);
+            return View(forum);
+        }
+
+        // GET: Forums/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var forum = await _context.Forums.FindAsync(id);
+            if (forum == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", forum.UserId);
+            return View(forum);
+        }
+
+        // POST: Forums/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ForumId,DateCreated,Title,Description,UserId")] Forum forum)
+        {
+            if (id != forum.ForumId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(forum);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ForumExists(forum.ForumId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", forum.UserId);
+            return View(forum);
+        }
+
+        // GET: Forums/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var forum = await _context.Forums
+                .Include(f => f.User)
+                .FirstOrDefaultAsync(m => m.ForumId == id);
+            if (forum == null)
+            {
+                return NotFound();
+            }
+
+            return View(forum);
+        }
+
+        // POST: Forums/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var forum = await _context.Forums.FindAsync(id);
+            _context.Forums.Remove(forum);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ForumExists(int id)
+        {
+            return _context.Forums.Any(e => e.ForumId == id);
+        }
+    }
+}
